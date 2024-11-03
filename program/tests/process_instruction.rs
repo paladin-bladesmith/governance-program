a7bd6aa3856be375a0894941152568649bcfcf0a
OliverNChalk <11343499+OliverNChalk@users.noreply.github.com>
2024-11-02 20:41:45 -0500

    process_instruction

--- program/tests/process_instruction.rs
+++ program/tests/process_instruction.rs
@@ -519,12 +519,13 @@ async fn success() {
     let stake_config_address = Pubkey::new_unique();
     let governance_config = GovernanceConfig {
         cooldown_period_seconds: 0,
-        proposal_acceptance_threshold: 0,
-        proposal_rejection_threshold: 0,
+        proposal_minimum_quorum: 0,
+        proposal_pass_threshold: 0,
         signer_bump_seed: 0,
         _padding: [0; 7],
         stake_config_address: stake_config_address,
         voting_period_seconds: 0,
+        stake_per_proposal: 0,
     };
 
     let treasury = get_treasury_address(&stake_config_address, &paladin_governance_program::id());
