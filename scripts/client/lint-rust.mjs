2fe58a45a125ed3fcea0c5798545dae3b4aff144
OliverNChalk <11343499+OliverNChalk@users.noreply.github.com>
2025-01-16 12:45:44 +1100

    cancer3

--- scripts/client/lint-rust.mjs
+++ scripts/client/lint-rust.mjs
@@ -10,7 +10,6 @@ import {
 // Configure additional arguments here, e.g.:
 // ['--arg1', '--arg2', ...cliArguments()]
 const lintArgs = [
-  '-Zunstable-options',
   '--',
   '--deny=warnings',
   ...cliArguments()
